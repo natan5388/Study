a80262922fbf0676961610b24fd54f9ea3074cd3
Natan5388 <35276299+natan5388@users.noreply.github.com>
2020-09-10 17:12:37 -0300

    Create lista_dina.py

---
+++ PythonBasics/lista_dina.py
@@ -0,0 +1,11 @@
+# Programa feito por Natan de moraes(natanmoraes5388@gmail.com)
+# Cria e add elementos a uma lista
+import os
+
+carros = ['l200', 'Hilux', 'Frontier']
+print(carros)       # É esperado que ele irá imprimir todos os membros da lista sem nenhuma formatação
+input("Press Enter to continue...")     # Pausa apos a impressão para que o usuario leia as informações
+os.system("cls")
+new_car = input('Add um modelo a lista: ')
+carros.append(new_car)      # Metodo append() adicona ao final da lista
+print(carros)
