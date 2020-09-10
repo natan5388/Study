03f73aafce04d3c4470940260779efe9afcc52ec
Natan5388 <35276299+natan5388@users.noreply.github.com>
2020-09-10 16:15:31 -0300

    Create Olá_mundo.py

---
+++ PythonBasics/Olá_mundo.py
@@ -0,0 +1,9 @@
+# Programa feito por Natan de moraes(natanmoraes5388@gmail.com)
+# Desc: Imprime olá mundo e mede o tempo de execução do programa
+import time
+#Inicio da função
+inicio = time.time()
+print('Olá, Mundo')
+fim = time.time()
+tempo = (fim - inicio)
+print('o tempo foi de: ',tempo)
