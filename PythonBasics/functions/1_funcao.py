d28d0c843a00d70b0ef45602b37d9b4ca33ebf09
Natan5388 <35276299+natan5388@users.noreply.github.com>
2020-10-06 17:35:18 -0300

    Create 1_funcao.py
    função não interativa

---
+++ PythonBasics/functions/1_funcao.py
@@ -0,0 +1,20 @@
+'''
+Arquivo: função_1.py
+Data: Tuesday, 6th October 2020 5:04 pm
+Autor: Natan De Moraes Borges (natanmoraes5388@gmail.com)
+Descrição: recebe um tamanho de camisa e frase e imprime via função
+'''
+
+def make_camisa(tamanho, frase):
+    '''
+    Possivel Parte Interativa
+    tamanho = int(input('Qual tamanho da camisa?\n>>> '))
+    frase = input('Qual frase gostaria de ter na camiseta?\n>>> ')
+    '''
+    int(tamanho)
+    print(f'O tamanho da camisa é: {tamanho}\n')
+    print(f'A frase escrita é: {frase}\n')
+'''
+make_camisa(30, 'Agora eu sei exatamente o que fazer')
+'''
+make_camisa(tamanho=35, frase='A vida me ensinou a nunca desistir, nem ganhar nem perder mas procurar evoluir')
